22ae5b196f639681fdff0b4399da79bc09ae86d3
Faisal-8898 <faaisal8898@gmail.com>
2024-07-06 11:50:44 +0600

    .

--- fullstack-backend.crud/src/main/java/com/faisal/fullstack_backend/crud/entity/User.java
+++ fullstack-backend.crud/src/main/java/com/faisal/fullstack_backend/crud/entity/User.java
@@ -4,44 +4,44 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@jakarta.persistence.Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    private String username;
-    private String name;
-    private String email;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    public Long getId() {
-        return id;
-    }
+  private String username;
+  private String name;
+  private String email;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
